652f12994eb1c88e5783f9f8c2684f15987422c6
lincer_x42 <tcoperator07@gmail.com>
2021-07-12 23:51:19 +0800

    fix missing return value and error field name

--- gee-web/v2-context/gee/context.go
+++ gee-web/v2-context/gee/context.go
@@ -23,17 +23,21 @@ func newContext(req *http.Request, w http.ResponseWriter) *context {
 
 //`Value` means retrieving the first value
 // another version: PostFormValue(), which excludes the url query part
-func (c *context) FormValue(key string) {
+func (c *context) FormValue(key string) string {
 	return c.Req.FormValue(key)
 }
 
 // url.Query() -> Values(type like map[string][]string)
 // Values.Get() returns first value in string format
 func (c *context) Query(key string) string {
-	return c.Req.url.Query().Get(key)
+	return c.Req.URL.Query().Get(key)
 }
 
 func (c *context) Status(code int) {
 	c.StatusCode = code
 	c.Writer.WriteHeader(code)
+}
+
+func (c *context) SetHeader(key, value string) {
+	c.Writer.Header().Set(key, value)
 }
