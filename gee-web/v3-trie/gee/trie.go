5c53d1bb4930c76772d781ae1ee8033912e797eb
lincer_x42 <tcoperator07@gmail.com>
2021-07-17 23:46:44 +0800

    begin trie structure

--- gee-web/v3-trie/gee/trie.go
+++ gee-web/v3-trie/gee/trie.go
@@ -1,1 +1,28 @@
 package gee
+
+type node struct {
+	totalPattern string
+	crtPattern   string // current node matching what
+	children     []*node
+	isExact      bool // if the current node is exact match
+}
+
+func (n *node) firstMatchInChildren(target string) *node {
+	for _, child := range n.children {
+		if child.crtPattern == target || !child.isExact {
+			return child
+		}
+	}
+	return nil
+}
+
+func (n *node) AllMatchesInChildren(target string) []*node {
+	nodes := make([]*node, 0)
+	for _, child := range n.children {
+		if child.crtPattern == target || !child.isExact {
+			nodes = append(nodes, child)
+		}
+	}
+	return nodes
+}
+
