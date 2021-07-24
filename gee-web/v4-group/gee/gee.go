edfcc7d9b96e49e3ad457bad62fba3c171ba92b5
lincer_x42 <tcoperator07@gmail.com>
2021-07-25 01:19:46 +0800

    finished v4 group control

--- gee-web/v4-group/gee/gee.go
+++ gee-web/v4-group/gee/gee.go
@@ -2,22 +2,39 @@ package gee
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 type HandleFunc func(c *Context)
 
-// embedding
-type Engine struct {
-	*RouterGroup
-	router *router
-	groups []*RouterGroup
-}
+/* the reason of `engine` field here:
+we want to give `routerGroup` the ability to create route policies,
+by adding the `engine` field, we can use routerGroup.engine.router.addRoute to achieve the goal.
+And we also want to keep the ability to directly control a route policy not related to one certain routerGroup,
+so we cannot just move the `router` to the inner of `RouterGroup`
+*/
 type RouterGroup struct {
 	prefix      string
 	middlewares []HandleFunc
-	parent      *RouterGroup
-	engine      *Engine
+	engine      *Engine // pointing to the only one global engine istance
+}
+
+/*Embedded struct
+type Engine struct {
+	rg *RouterGroup
+	router *Router
+	groups []*RouterGroup
+}
+func (engine *Engine) GET(path string, handler HandlerFunc) {
+	engine.rg.addRoute("GET", pattern, handler)
+}
+*/
+
+type Engine struct {
+	*RouterGroup // embeded type
+	router       *router
+	groups       []*RouterGroup
 }
 
 func NewEngine() *Engine {
@@ -27,16 +44,28 @@ func NewEngine() *Engine {
 	return engine
 }
 
-func (e *Engine) addRoute(method string, path string, handler HandleFunc) {
-	e.router.addRoute(method, path, handler)
+func (group *RouterGroup) NewGroup(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
 }
 
-func (e *Engine) GET(path string, handle HandleFunc) {
-	e.addRoute("GET", path, handle)
+func (group *RouterGroup) addRoute(method string, urlPattern string, handler HandleFunc) {
+	pattern := group.prefix + urlPattern
+	log.Printf("pattern = %s\n", pattern)
+	group.engine.router.addRoute(method, pattern, handler)
 }
 
-func (e *Engine) POST(path string, handle HandleFunc) {
-	e.addRoute("POST", path, handle)
+func (group *RouterGroup) GET(pattern string, handler HandleFunc) {
+	group.addRoute("GET", pattern, handler)
+}
+
+func (group *RouterGroup) POST(pattern string, handler HandleFunc) {
+	group.addRoute("POST", pattern, handler)
 }
 
 func (e *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
--- gee-web/v4-group/main.go
+++ gee-web/v4-group/main.go
@@ -12,26 +12,33 @@ func main() {
 	r.GET("/", func(c *gee.Context) {
 		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
 	})
-	r.GET("/hello", func(c *gee.Context) {
-		// expect /hello?name=arthur
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
-	r.GET("/hello/:name", func(c *gee.Context) {
-		// expect /hello/arthur
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
-
-	r.GET("/assets/*filepath", func(c *gee.Context) {
-		// expect /assets/css/index.css
-		c.JSON(http.StatusOK, gee.JSONObj{"filepath": c.Param("filepath")})
-	})
-
-	r.POST("/login", func(c *gee.Context) {
-		c.JSON(http.StatusOK, gee.JSONObj{
-			"username": c.FullFormValue("username"),
-			"password": c.FullFormValue("password"),
+	v1 := r.NewGroup("/v1")
+	{
+		v1.GET("/hello", func(c *gee.Context) {
+			// expect /hello?name=arthur
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 		})
-	})
+		v1.GET("/hello/:name", func(c *gee.Context) {
+			// expect /hello/arthur
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+	}
+
+	v2 := r.NewGroup("/v2")
+	{
+		v2.GET("/assets/*filepath", func(c *gee.Context) {
+			// expect /assets/css/index.css
+			c.JSON(http.StatusOK, gee.JSONObj{"filepath": c.Param("filepath")})
+		})
+
+		authNode := v2.NewGroup("/auth")
+		authNode.POST("/login", func(c *gee.Context) {
+			c.JSON(http.StatusOK, gee.JSONObj{
+				"username": c.FullFormValue("username"),
+				"password": c.FullFormValue("password"),
+			})
+		})
+	}
 
 	r.Run(":9999")
 }
